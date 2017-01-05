8bfc7195ffddcd7b8d5192a13a54149563d3e397
serstep <myserstep@mail.ru>
2017-01-05 13:23:19 +0200

    Update format_price.py

--- format_price.py
+++ format_price.py
@@ -1,5 +1,29 @@
+import re, sys
+
+
 def format_price(price):
-    pass
+
+    if re.fullmatch(r"([\d]{1,10}\.[\d]{1,6})", price) is None:
+        return None
+
+    aparted_price = price.split(".")
+    integer_part = int(aparted_price[0])
+    fractional_part = int(aparted_price[1][:2])
+
+    if fractional_part == 0 :
+        return "{:,}".format(integer_part).replace(",", " ")
+
+    return "{:,}.{:02}".format(integer_part, fractional_part).replace(",", " ")
+
 
 if __name__ == '__main__':
-    pass
+    if len(sys.argv) < 2:
+        print("Put price string as parameter")
+        exit(1)
+
+    formatted_price = format_price(sys.argv[1])
+    if formatted_price is None:
+        print("Invalid price string")
+        exit(1)
+
+    print(formatted_price)

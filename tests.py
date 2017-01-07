836d83ccf69fa2a26ef8bed568383b492b01a1a7
serstep <myserstep@mail.ru>
2017-01-07 17:24:29 +0200

    Update tests.py

--- tests.py
+++ tests.py
@@ -5,22 +5,27 @@ class TestPriceFormat(unittest.TestCase):
 
     def test_valid_full_float_part(self):
         formatted_price = format_price("37498234.49")
-        self.assertIsNotNone(formatted_price)
+        self.assertEqual(formatted_price, "37 498 234.49")
         
 
-    def test_invalid_without_float_part(self):
+    def test_valid_without_float_part(self):
         formatted_price = format_price("65665")
-        self.assertIsNone(formatted_price)
+        self.assertEqual(formatted_price, "65 665")
         
 
     def test_valid_zero_float_part(self):
         formatted_price = format_price("11.0")
-        self.assertIsNotNone(formatted_price)
+        self.assertEqual(formatted_price, "11")
         
 
     def test_valid_zero_after_dot(self):
         formatted_price = format_price("7384934.07983")
-        self.assertIsNotNone(formatted_price)
+        self.assertEqual(formatted_price, "7 384 934.07")
+
+
+    def test_invalid_dot_without_float_part(self):
+        formatted_price = format_price("11.")
+        self.assertIsNone(formatted_price)
         
 
     def test_invalid_string_chars_in_int_part(self):
@@ -47,11 +52,11 @@ class TestPriceFormat(unittest.TestCase):
         formatted_price = format_price("9783634823.34.798")
         self.assertIsNone(formatted_price)
     
-    
+
     def test_invalid_negative(self):
         formatted_price = format_price("-34834.798")
         self.assertIsNone(formatted_price)
-        
+
 
 if __name__ == "__main__":
 
